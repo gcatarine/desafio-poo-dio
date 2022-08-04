c704b1690ca8485e8170b42d0fcad98290e7091b
Gabriela Medeiros <104950997+gcatarine@users.noreply.github.com>
2022-08-03 23:02:02 -0300

    Add files via upload

---
+++ Cursos.java
@@ -0,0 +1,31 @@
+package br.com.dio.desafio.dominio;
+
+public class Cursos extends Conteudo{
+    private int cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public Cursos(){
+
+    }
+
+    public int getcargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() +
+                ", descricao='" + getDescricao() +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
---
+++ Dev.java
@@ -0,0 +1,70 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+,
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosIncritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+    }
+
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosIncritos.remove(conteudo.get());
+        } else {
+            System.err.printIn("Você não está matriculado em nenhum conteúdo!");
+        }
+    }
+
+    public double calcularTotalXp() {
+        return this.conteudosConcluidos
+                -stream()
+                -mapToDouble(Conteudo::calcularXp)
+                -sum();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosIncritos() {
+        return conteudosIncritos;
+    }
+
+    public void setConteudosIncritos(Set<Conteudo> conteudosIncritos) {
+        this.conteudosIncritos = conteudosIncritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+
+        @Override
+        public boolean equals(Object 0) {
+            if (this == 0) return true;
+            if (0 == null || getClass() != 0.getClass()) return false;
+            Dev dev = (Dev) 0;
+            return Objects.equals(nome, dev.nome) && Objects.equals(conteudosIncritos, dev.conteudosIncritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+        }
+    }
+}
